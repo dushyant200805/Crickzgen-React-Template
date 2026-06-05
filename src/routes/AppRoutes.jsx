363318887fda80638723c7aac6d1943db437309d
dushyant200805 <dush8387@gmail.com>
2026-06-05 12:27:36 +0530

    update

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,11 +5,11 @@ import Footer from "../components/Layout/Footer";
 
 import Home from "../pages/home/Home";
 import AboutPage from "../pages/about/AboutPage";
-import MatchesPage from "../pages/matche/matchesPage";
-import ContactPage from "../pages/contact/contactPage";
+import MatchesPage from "../pages/matche/MatchesPage";
+import ContactPage from "../pages/contact/ContactPage";
 import BlogPage from "../pages/blog/BlogPage";
 import BlogDetailsPage from "../pages/blogdetail/BlogDetailsPage";
-import GalleryPage from "../pages/gallery/galleryPage";
+import GalleryPage from "../pages/gallery/GalleryPage";
 import NotFound from "../pages/notfound/NotFound";
 
 import ScrollFunction from "../components/Common-Sections/ScrollFunction";
